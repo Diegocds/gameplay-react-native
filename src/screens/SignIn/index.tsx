b069cbe756d506cfd96ba53a89999e4579f60df5
Diegocds <diegocds1990@gmail.com>
2021-06-22 13:57:47 -0300

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import {  Text, View, Image } from 'react-native';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { styles } from './styles'
+import IllustrationImg from '../../assets/illustration.png'
+
+export  function SignIn() {
+    const [text, setText] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Image 
+      source={IllustrationImg} 
+      style={styles.image}
+      resizeMode="stretch"
+      />
+
+        <View style={styles.content}>
+            <Text style={styles.title}>
+                Conecte-se {'\n'}
+                e organize suas {'\n'}
+                jogatinas
+            </Text>
+
+            <Text style={styles.subtitle}>
+                Crie grupos para jogar seus games{'\n'}
+                favoritos com seus amigos
+            </Text>
+
+            <ButtonIcon 
+            title="Entrar com Discord"
+            activeOpacity={0.7}
+            />
+        </View>
+
+    </View>
+  );
+}
+
+
